cb8a3462b8af2a8dcf0fcfd5bdab5073c4290895
zhenzhu143321 <hxcisunli@126.com>
2019-02-28 20:03:18 +0800

    ECSWithJobSystem性能测试
    ECSWithJobSystem性能测试

---
+++ Assets/Scripts/04ECSWithJobSystem/MovementSystem.cs
@@ -0,0 +1,47 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace ECSWithJobSystemMethod
+{
+    public class MovementSystem : JobComponentSystem
+    {
+        [BurstCompile]
+        struct MovementJob : IJobProcessComponentData<Position, Rotation, MoveSpeed>
+        {
+            public float topBound;
+            public float bottomBound;
+            public float deltaTime;
+
+            public void Execute(ref Position position, [ReadOnly] ref Rotation rotation, [ReadOnly] ref MoveSpeed speed)
+            {
+                float3 value = position.Value;
+
+                value += deltaTime * speed.Value * math.forward(rotation.Value);
+
+                if (value.z < bottomBound)
+                    value.z = topBound;
+
+                position.Value = value;
+            }
+        }
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            MovementJob moveJob = new MovementJob
+            {
+                topBound =200,
+                bottomBound = -200,
+                deltaTime = Time.deltaTime
+            };
+
+            JobHandle moveHandle = moveJob.Schedule(this,inputDeps);
+
+            return moveHandle;
+           
+        }
+    }
+}
